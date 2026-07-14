7bc02218085a2472a2d58115f1aec9491e6d1e2e
Sofosss <argsofos.1@gmail.com>
2026-07-15 02:42:29 +0300

    Update setup files

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+pip install .
+pip install copulas==0.14.1 --no-deps
+pip install ctgan==0.12.1 --no-deps
+pip install deepecho==0.8.1 --no-deps
+pip install rdt==1.21.0 --no-deps
+pip install sdmetrics==0.28.0 --no-deps
+pip install sdv==1.33.0 --no-deps
+pip install statsmodels==0.14.6 --no-deps
