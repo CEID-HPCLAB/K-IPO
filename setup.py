1eec441fc74b4943c9d43f7b54d807093371db7a
Sofosss <argsofos.1@gmail.com>
2026-07-14 16:35:03 +0300

    Update setup script, add toml file for package installation

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-def reqs():
-    with open("requirements.txt") as f:
-        return [
-            line.strip()
-            for line in f
-            if line.strip() and not line.startswith("-e")
-        ]
-
-setup(
-    name = "kipo",
-    version = "0.1.0",
-    packages = find_packages(where = "src"),
-    package_dir = {"": "src"},
-    install_requires = reqs(),
-    python_requires=">=3.8,<=3.10",
-)
+setup()
